86d52e5fbd886a824836bbedc190c96f7d6fc1c8
GlobusShwobus <fjodorzaro@gmail.com>
2025-06-27 22:40:15 +0300

    camera - does not scale yet, because we dealing with rects

--- recomp/Camera.h
+++ recomp/Camera.h
@@ -1,48 +1,49 @@
 #pragma once
-#include "SDL3/SDL.h"
 
 namespace badEngine {
 
-	class ScreenSpace {
-
+	class Camera2D {
 		float offsetX = 0.0f;
 		float offsetY = 0.0f;
+		float scaleX = 1.0f;
+		float scaleY = 1.0f;
 
-		float panX = 0.0f;
-		float panY = 0.0f;
-
-		float scaleX = 0.0f;
-		float scaleY = 0.0f;
+		int screenW = 0;
+		int screenH = 0;
 
 	public:
 
-		void cWorldToScreen(float worldX, float worldY, float& screenX, float& screenY)const
-		{
+		Camera2D(int screenW, int screenH) :screenW(screenW), screenH(screenH) {
+			centerOn(0.0f, 0.0f);
+		}
+
+		void setScreenSize(int width, int height) {
+			screenW = width;
+			screenH = height;
+		}
+		void centerOn(float worldX, float worldY) {
+			offsetX = worldX - (screenW / 2.0f) / scaleX;
+			offsetY = worldY - (screenH / 2.0f) / scaleY;
+		}
+		void move(float dx, float dy) {
+			offsetX += dx / scaleX;
+			offsetY += dy / scaleY;
+		}
+		void zoom(float factor) {
+			scaleX *= factor;
+			scaleY *= factor;
+		}
+		void worldToScreen(float worldX, float worldY, float& screenX, float& screenY) const {
 			screenX = (worldX - offsetX) * scaleX;
 			screenY = (worldY - offsetY) * scaleY;
 		}
-		void cScreenToWorld(float screenX, float screenY, float& worldX, float& worldY)const
-		{
+		void screenToWorld(float screenX, float screenY, float& worldX, float& worldY) const {
 			worldX = screenX / scaleX + offsetX;
 			worldY = screenY / scaleY + offsetY;
 		}
-	};
 
-	class Camera : public ScreenSpace {
-
-	public:
-		SDL_FPoint worldToScreen(const SDL_FPoint& inWorldPos)const
-		{
-			SDL_FPoint iwp = inWorldPos;
-			cWorldToScreen(inWorldPos.x, inWorldPos.y, iwp.x, iwp.y);
-			return iwp;
-		}
-		SDL_FPoint screenToWorld(const SDL_FPoint& inScreenPos)const
-		{
-			SDL_FPoint isp = inScreenPos;
-			cScreenToWorld(inScreenPos.x, inScreenPos.y, isp.x, isp.y);
-			return isp;
-		}
+		float getScaleX() const { return scaleX; }
+		float getScaleY() const { return scaleY; }
 	};
 
 }
